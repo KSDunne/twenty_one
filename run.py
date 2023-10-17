b17f3ed9dd300f68d339803ac26e627aa7b279cb
KSDunne <77549699+KSDunne@users.noreply.github.com>
2023-10-17 14:48:26 +0100

    Add total hand function that takes the aces bust phenomenon into account

--- run.py
+++ run.py
@@ -52,3 +52,36 @@ def deal_cards(deck, player):
     card = deck.pop()
     player.append(card)
     return card
+
+
+def total(hand):
+    # Compute the total of hand
+
+    values = {
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "1": 10,
+        "J": 10,
+        "Q": 10,
+        "K": 10,
+        "A": 11,
+    }
+
+    result = 0
+    num_aces = 0
+    for card in hand:
+        result += values[card[0]]
+        if card[0] == "A":
+            num_aces += 1
+
+    while result > 21 and num_aces > 0:
+        result -= 10
+        num_aces -= 1
+
+    return result
