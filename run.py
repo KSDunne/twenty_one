3eb0bf78f6094032ec18b0099e7c8411e8955bd4
KSDunne <77549699+KSDunne@users.noreply.github.com>
2023-10-30 20:19:47 +0000

    Change the word rule to uppercase

--- run.py
+++ run.py
@@ -116,18 +116,18 @@ def total(hand):
 
 def rules():
     print("Rules for playing the game:\n")
-    print("Rule1: Have a hand that totals higher than the dealer's, but is not > 21\n")
+    print("RULE 1: Have a hand that totals higher than the dealer's, but is not > 21\n")
     print(
-        "Rule2: Number cards are worth face value, colored cards (i.e. J, Q and K) are 10 and an ace can be 1 or 11\n"
+        "RULE 2: Number cards are worth face value, colored cards (i.e. J, Q and K) are 10 and an ace can be 1 or 11\n"
     )
     print(
-        "Rule3: Hit means you take another card, stand means you keep your current hand\n"
+        "RULE 3: Hit means you take another card, stand means you keep your current hand\n"
     )
     print(
-        "Rule4: If player has > 21, they bust and lose. If the dealer has > 21, they also bust and the player wins\n"
+        "RULE 4: If player has > 21, they bust and lose. If the dealer has > 21, they also bust and the player wins\n"
     )
     print(
-        "Rule5: The dealer must hit if they are < 17, unless the player decided to stand and the dealer already has > the player. The dealer wins in this instance\n"
+        "RULE 5: The dealer must hit if they are < 17, unless the player decided to stand and the dealer already has > the player. The dealer wins in this instance\n"
     )
     print(
         "Note: The game here is the basic version of blackjack. It is good for beginners to learn the core game. This is why it was renamed to TwentyOne. Enjoy!\n"
