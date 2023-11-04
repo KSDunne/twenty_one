eeee50403b4cfb37760dff3b5c3128a331eb8673
KSDunne <77549699+KSDunne@users.noreply.github.com>
2023-11-04 15:00:40 +0000

    Move rules to separate file

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import sys
 from os import system, name
 from colorama import Fore  # color styling
 from spreadsheets import worksheet
+from rules import print_rules
 
 WELCOME_MESSAGE = "WELCOME TO TWENTYONE"
 DEFAULT_MESSAGE = "(H)it, (S)tand or (R)ules? (ENTER means hit):\nUser input: "
@@ -116,31 +117,6 @@ def total(hand):
     return result
 
 
-def rules():
-    """
-    This prints 5 basic rules of the game to the screen. The game presented here is a simple
-    version of blackjack and so it was renamed to twenty-one.
-    """
-
-    print("Rules for playing the game:\n")
-    print("RULE 1: Have a hand that totals higher than the dealer's, but is not > 21\n")
-    print(
-        "RULE 2: Number cards are worth face value, colored cards (i.e. J, Q, K) are 10 and an ace can be 1 or 11\n"
-    )
-    print(
-        "RULE 3: Hit means you take another card, stand means you keep your current hand\n"
-    )
-    print(
-        "RULE 4: If player has > 21, they bust and lose. If the dealer has > 21, they also bust and the player wins\n"
-    )
-    print(
-        "RULE 5: The dealer must hit if they are < 17, unless the player decided to stand and the dealer already has > the player. The dealer wins in this instance\n"
-    )
-    print(
-        "Note: The game here is the basic version of blackjack. It is good for beginners to learn the core game. This is why it was renamed to Twenty-One. Enjoy!\n"
-    )
-
-
 def increment_wins():
     """
     This function increments the wins column for the user in google sheets.
@@ -226,7 +202,7 @@ def twenty_one():
         answer = input(DEFAULT_MESSAGE)
     while answer in {"", "h", "hit", "s", "S", "r", "R"}:
         if answer in {"r", "R"}:
-            rules()
+            print_rules()
         elif answer in {"s", "S"}:
             break
         else:
@@ -260,7 +236,7 @@ def main_menu():
     answer = input(MAIN_MENU_MESSAGE)
     while answer not in {"q", "Q"}:
         if answer in {"r", "R"}:
-            rules()
+            print_rules()
         if answer in {"l", "L"}:
             show_scoreboard()
         elif answer in {"n", "N"}:
@@ -270,7 +246,7 @@ def main_menu():
             if replay in {"n", "N"}:
                 answer = input(MAIN_MENU_MESSAGE)
                 if answer in {"r", "R"}:
-                    rules()
+                    print_rules()
                 if answer in {"l", "L"}:
                     show_scoreboard()
                 if answer in {"q", "Q"}:
