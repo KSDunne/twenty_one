049cd915c6061aac73fcb4039f6118d60c8d2107
KSDunne <77549699+KSDunne@users.noreply.github.com>
2023-11-05 18:28:07 +0000

    Fix minor line too long errors in messages file

--- messages.py
+++ messages.py
@@ -10,15 +10,17 @@ PLAYER_BUST_MESSAGE = "You bust, sorry "
 HOUSE_BUST_MESSAGE = "House bust, you win "
 REPLAY_MESSAGE = "Play another game? (Y)es or (N)o\nUser input: "
 GOODBYE_MESSAGE = "Goodbye, "
-MAIN_MENU_MESSAGE = "(R)ules\n(N)ew game\n(L)eaderboard\n(Q)uit\n\nUser input: "
+MAIN_MENU_MESSAGE = "(R)ules\n(N)ew game\n(L)eaderboard\n(Q)uit\n\n" +
+"User input: "
 
 
 # Credit: https://github.com/kpsdev1/blackjack/blob/main/run.py
 def notification(text):
     """
-    This is a notification function for gameplay user feedback. pyfiglet takes ASCII text
-    and renders it in ASCII art fonts. The font used here is called big. The art is displayed on
-    the start screen. It is also displayed on the win, lose and goodbye screens.
+    This is a notification function for gameplay user feedback. pyfiglet takes
+    ASCII text and renders it in ASCII art fonts. The font used here is called
+    big. The art is displayed on the start screen. It is also displayed on the
+    win, lose and goodbye screens.
     """
     print(pyfiglet.figlet_format(text, font="big"))
 
@@ -26,8 +28,9 @@ def notification(text):
 # Credit: https://github.com/luizsmania/blackjack/blob/main/run.py#L49C1-L59C28
 def clear():
     """
-    Here is a function for clearing the terminal. This function is good for user experience
-    during gameplay, as the terminal backlog can look confusing.
+    Here is a function for clearing the terminal. This function is good for
+    user experience during gameplay, as the terminal backlog can look
+    confusing.
     """
     # for windows
     if name == "nt":
